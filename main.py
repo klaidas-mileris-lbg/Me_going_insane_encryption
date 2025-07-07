ae0ccd73362ddc9d01e68e519d1488cfb1d484ff
klaidas-mileris-lbg <klaidas.mileris@lloydsbanking.com>
2025-07-07 12:32:40 +0100

    just a slight useless update1

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# True security can't rely on the source code being secret. It has to rely on the complexity and the impossibility of reverse-engineering the final result due
+# to its mathematical properties.
+
+
 def dec_to_binary(dec_num: int):
     remainder = 0
     if dec_num == 0:
@@ -13,7 +17,7 @@ def dec_to_binary(dec_num: int):
     if current_number == 1:
         binary_number.append(str(current_number))
     return "".join(binary_number[::-1])
-print(dec_to_binary(89))
+# print(dec_to_binary(89))
 
 def generate_random_key(length_of_key):
     """generates a random binary key of the specified length using user-generated entropy"""
@@ -44,7 +48,7 @@ def generate_random_key(length_of_key):
     print("".join(random_key))
     
 
-generate_random_key(128)
+# generate_random_key(128)
 
 
 def bitwise_XOR(binary_number: str, generated_key: str):
@@ -57,4 +61,60 @@ def bitwise_XOR(binary_number: str, generated_key: str):
                 new_result += "1"
     return new_result
 
-print(bitwise_XOR("111001", "001010"))
+# print(bitwise_XOR("111001", "001010"))
+
+
+
+
+
+
+def generate_mouse_movement_entropy(time_for_collection):
+    """Prompts the user to move the mouse and generates random numbers from that"""
+    import pyautogui
+    import time
+    import secrets
+    import hashlib
+    print(f"In 2 seconds, imagine a picture in your head and move your mouse, pretend to draw and colour in" 
+          f"that picture for {time_for_collection} seconds to generate entropy...")
+    time.sleep(2)
+    entropy_list = []
+    print("Collecting entropy...")
+    for i in range(time_for_collection*10):
+        mouse_x, mouse_y = pyautogui.position()
+        new_number = (mouse_x * secrets.randbelow(500)) * (mouse_y * secrets.randbelow(500))
+        hasher = hashlib.sha256()
+        new_number_bytes = str(new_number).encode("utf-8")
+        # hashes the number
+        hasher.update(new_number_bytes)
+        # now we do bits > bytes > dec
+        new_number_bytes = hasher.digest()
+        new_number_dec = int.from_bytes(new_number_bytes, "big")
+        entropy_list.append(str(new_number_dec))
+        time.sleep(0.1)
+        print(new_number_dec)
+    return entropy_list
+     
+
+
+def generating_random_key(key_len, time_for_collection):
+    import secrets
+    # this time let's make something that is actually truly random
+    # so this gives us a list of quite random numbers
+    final_key = ""
+    entropy_list = generate_mouse_movement_entropy(time_for_collection)
+    for i in range(key_len):
+        random_number: str = entropy_list[secrets.randbelow(len(entropy_list))]
+        random_digit: int = int(random_number[secrets.randbelow(len(random_number))])
+        if random_digit % 2 == 0:
+            final_key += "1"
+        else:
+            final_key += "0"
+    return final_key
+
+    
+
+
+
+print(f"The new secret key: {generating_random_key(128, 2)}")
+
+# just a quick test
